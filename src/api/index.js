a798b75969379204b78f875497ca48c9eb1fb599
bbennett7 <bryn.bennett@gmail.com>
2020-04-17 09:53:37 -0700

    Update master list plan

--- src/api/index.js
+++ src/api/index.js
@@ -26,7 +26,21 @@ const getMasterListPlans = async () => {
   }
 };
 
+const updateMasterListPlan = async (body, id) => {
+  try {
+    return axios.put(`${process.env.REACT_APP_API_URL}/plans/${id}`, body, {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    });
+  } catch (err) {
+    return err;
+  }
+};
+
 export default {
   getPendingPlans,
-  getMasterListPlans
+  getMasterListPlans,
+  updateMasterListPlan
 };
