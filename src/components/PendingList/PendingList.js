453b9b0197f3493b540079a9c447bf6d2a00c3b2
bbennett7 <bryn.bennett@gmail.com>
2020-04-16 22:38:14 -0700

    Handle on click pending item

--- src/components/PendingList/PendingList.js
+++ src/components/PendingList/PendingList.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import styles from './PendingList.module.scss';
 
-const PendingList = ({ items, setActiveItem }) => {
+const PendingList = ({ items, setActiveItem, itemsType, activeId }) => {
   const handleOnClick = event => {
     event.preventDefault();
     const { id } = event.currentTarget;
-    console.log('clicked', id);
-    return setActiveItem(id);
+    const item = items.find(i => i[itemsType].id == id);
+
+    return setActiveItem(item);
   };
 
   const renderPendingList = () => {
     return items.map(item => {
       const { provider, plan } = item;
+      const className = activeId === item[itemsType].id ? styles.active : null;
+
       return (
-        <div className={styles.item} key={plan.id} id={plan.id} onClick={handleOnClick}>
+        <div
+          className={`${styles.item} ${className}`}
+          key={plan.id}
+          id={plan.id}
+          onClick={handleOnClick}
+        >
           {provider.provider_name} - {plan.plan_name}
         </div>
       );
