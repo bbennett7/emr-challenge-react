bb81fb6b5db21d3d9e3c06f99a1456b8a921e323
bbennett7 <bryn.bennett@gmail.com>
2020-04-16 21:30:48 -0700

    Fix useEffect fetches, render pending list

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,30 +2,31 @@ import React, { useState, useEffect } from 'react';
 import styles from './Home.module.scss';
 import API from '../../api/index';
 import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
+import PendingList from '../../components/PendingList/PendingList';
 
 const Home = () => {
   const [loading, setLoadingState] = useState(true);
   const [pendingPlans, setPendingPlans] = useState([]);
   const [masterListPlans, setMasterListPlans] = useState([]);
+  const [activePlanId, setActivePlanId] = useState({});
 
   useEffect(() => {
     if (loading) {
-      getPendingPlans();
-      getMasterListPlans();
-
-      setLoadingState(false);
+      fetchData();
     }
   });
 
-  const getPendingPlans = async () => {
-    const data = await API.getPendingPlans();
-    setPendingPlans(data.data.pendingPlans);
-  };
+  const fetchData = async () => {
+    const pendingPlanData = await API.getPendingPlans();
+    const { pendingPlans } = pendingPlanData.data;
+    setPendingPlans(pendingPlans);
+    setActivePlanId(pendingPlans[0].plan.id);
 
-  const getMasterListPlans = async () => {
-    const data = await API.getMasterListPlans();
-    setMasterListPlans(data.data.plans);
-  }
+    const masterPlanData = await API.getMasterListPlans();
+    setMasterListPlans(masterPlanData.data.plans);
+
+    setLoadingState(false);
+  };
 
   if (loading) {
     return (
@@ -35,7 +36,15 @@ const Home = () => {
     );
   }
 
-  return <div className={styles.container}></div>;
+  if (pendingPlans.length === 0) {
+    return <div className={styles.container}>No pending plans to approve.</div>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <PendingList items={pendingPlans} setActiveItem={setActivePlanId} />
+    </div>
+  );
 };
 
 export default Home;
