591f41f744c9023069d0dd56bb9b0ee4a377fac9
bbennett7 <bryn.bennett@gmail.com>
2020-04-16 20:36:59 -0700

    Get pending plans api call

---
+++ src/api/index.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+const getPendingPlans = async () => {
+  try {
+    return axios.get(`${process.env.REACT_APP_API_URL}/plans/pending`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    });
+  } catch (err) {
+    return err;
+  }
+};
+
+export default {
+  getPendingPlans
+};
