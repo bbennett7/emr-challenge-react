d50348e62b64dca68a7bf2d10641ab7a0aee5421
bbennett7 <bryn.bennett@gmail.com>
2020-04-16 20:38:10 -0700

    Get pending plans on home mount, set loading spinner if loading

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Home.module.scss';
+import API from '../../api/index';
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
 
 const Home = () => {
+  const [loading, setLoadingState] = useState(true);
+  const [pendingPlans, setPendingPlans] = useState([]);
+  const [masterListPlans, setMasterListPlans] = useState([]);
+
+  useEffect(() => {
+    if (loading) {
+      getPendingPlans();
+
+      setLoadingState(false);
+    }
+  });
+
+  const getPendingPlans = async () => {
+    const data = await API.getPendingPlans();
+    setPendingPlans(data.data.pendingPlans);
+  };
+
+  if (loading) {
+    return (
+      <div className={styles.container}>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
   return <div className={styles.container}></div>;
 };
 
