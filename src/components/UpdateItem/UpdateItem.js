72f8602325d885875fabf1086882d85991806b69
bbennett7 <bryn.bennett@gmail.com>
2020-04-17 09:54:07 -0700

    Add useeffect and handle on change/on submit to update item component

--- src/components/UpdateItem/UpdateItem.js
+++ src/components/UpdateItem/UpdateItem.js
@@ -1,28 +1,93 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './UpdateItem.module.scss';
 import Button from '../Button/Button';
+import API from '../../api/index';
 
-const UpdateItem = ({ activePlan }) => {
+const UpdateItem = ({ activeItem, removeApprovedItem }) => {
+  const { plan } = activeItem;
+  const [loadingStatus, setLoadingStatus] = useState(true);
+  const [nameInput, setNameInput] = useState(plan.plan_name);
+  const [typeInput, setTypeInput] = useState(plan.plan_type);
+  const [originalName, setOriginalName] = useState(plan.plan_name);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    if (!loadingStatus && originalName !== plan.plan_name) {
+      setOriginalName(plan.plan_name);
+    }
+
+    if (originalName !== plan.plan_name) {
+      setNameInput(plan.plan_name);
+      !plan.plan_type ? setTypeInput('') : setTypeInput(plan.plan_type);
+    }
+
+    setLoadingStatus(false);
+  });
+
+  const handleOnChange = event => {
+    event.preventDefault();
+    const { id, value } = event.currentTarget;
+
+    if (id === 'name') {
+      setNameInput(value);
+    }
+
+    if (id === 'type') {
+      setTypeInput(value);
+    }
+  };
+
+  const handleOnSubmit = async event => {
+    event.preventDefault();
+    const body = {
+      plan_name: nameInput,
+      plan_type: typeInput
+    };
+
+    try {
+      const d = await API.updateMasterListPlan(body, plan.id);
+      console.log(d);
+      removeApprovedItem(plan.id);
+    } catch (err) {
+      return setErrorMessage(err.message);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>Update Item</div>
       <div className={styles.description}>
         Making changes to this plan will update the plan in the system&apos;s master list. The
-        source&apos;s mapping entry will not be changed.
+        source&apos;s mapping entry will not be changed. Approving without making changes approves
+        the entry as is.
       </div>
 
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleOnSubmit}>
         <label>
           Plan Name
-          <input type="text" />
+          <input
+            type="text"
+            id={'name'}
+            value={nameInput}
+            defaultValue={nameInput}
+            onChange={handleOnChange}
+          />
         </label>
 
         <label>
           Plan Type
-          <input type="text" />
+          <input
+            type="text"
+            id={'type'}
+            value={typeInput}
+            defaultValue={typeInput}
+            onChange={handleOnChange}
+          />
         </label>
 
-        <Button type="submit" />
+        <Button type="submit" onClick={handleOnSubmit} />
+
+        {errorMessage === '' ? null : <div className={styles.error}>{errorMessage}</div>}
       </form>
     </div>
   );
